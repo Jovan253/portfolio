476c21484b468ebcbac01a61037e8b2d5c37cea4
Jovan253 <jhadzic253@gmail.com>
2026-03-07 17:41:46 +0000

    fix: re-work sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faMedal, faCircleInfo, faPenRuler } from "@fortawesome/free-solid-svg-icons";
+
 export default function Sidebar() {    
     return (
         <aside className="sidebar">
@@ -6,12 +11,17 @@ export default function Sidebar() {
                 Jovan Hadzic
             </div>
 
-            <nav>
-                <a href="#projects">Projects</a>
-                <a href="/about">About</a>
-                <a href="/certifications">Certifications</a>
-                <a href="https://github.com/Jovan253">GitHub</a>
-                <a href="https://www.linkedin.com/in/jovan-hadzic-6644321ba/">LinkedIn</a>
+            <nav className="sidebar-nav">
+                <Link to="/"><FontAwesomeIcon icon={faPenRuler}/> Projects</Link>
+                <Link to="/"><FontAwesomeIcon icon={faCircleInfo}/> About</Link>
+                <Link to="/"><FontAwesomeIcon icon={faMedal}/> Certifications</Link>
+                
+                <a href="https://github.com/Jovan253" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub}/> Github
+                </a>
+                <a href="https://www.linkedin.com/in/jovan-hadzic-6644321ba/" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin}/> Linkedin
+                </a>
             </nav>
 
         </aside>
